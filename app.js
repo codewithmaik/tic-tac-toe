4258020c6cc3fec7c33474368ffda3f6561d63dd
codewithmaik <mcawelius@web.de>
2023-03-31 14:21:18 +0000

    Create function to create board

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const info = document.querySelector("#info");
 const startCells = [
     "", "", "", "", "", "", "", "", ""
 ]
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div")
+        cellElement.classList.add("square")
+    })
+}
