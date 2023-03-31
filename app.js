fd170d42da3ea44bca7039cdd622e107e718469e
codewithmaik <mcawelius@web.de>
2023-03-31 14:50:57 +0000

    Add append method to createBoard function and call iot

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const gameboard = document.querySelector("#gameboard");
-const info = document.querySelector("#info");
+const gameBoard = document.querySelector("#gameboard");
+const infoDisplay = document.querySelector("#info");
 
 const startCells = [
     "", "", "", "", "", "", "", "", ""
@@ -9,5 +9,8 @@ function createBoard() {
     startCells.forEach((cell, index) => {
         const cellElement = document.createElement("div")
         cellElement.classList.add("square")
+        gameBoard.append(cellElement)
     })
 }
+
+createBoard()
