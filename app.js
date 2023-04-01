7b1ddd5f2f4850c99e1b21124b0915b35cc359cf
codewithmaik <mcawelius@web.de>
2023-04-01 19:32:17 +0000

    Add addGo function

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const startCells = [
     "", "", "", "", "", "", "", "", ""
 ]
 
+let go = "circle"
+infoDisplay.textContent = "Circle goes first"
+
 function createBoard() {
     startCells.forEach((cell, index) => {
         const cellElement = document.createElement("div")
@@ -15,3 +18,12 @@ function createBoard() {
 }
 
 createBoard()
+
+function addGo(e) {
+    const goDisplay = document.createElement("div")
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = `it is now ${go}'s turn.`
+    e.target.removeEventlistener("click", addGo)
+}
