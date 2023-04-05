c407cd272f15748a86eca70c31862b262b199f62
codewithmaik <mcawelius@web.de>
2023-04-05 21:20:13 +0000

    Comment checkScore function to improve maintainibility

--- app.js
+++ app.js
@@ -34,13 +34,15 @@ function addGo(e) {
 }
 
 function checkScore() {
+    // collect all squares
     const allSquares = document.querySelectorAll(".square")
+    // define all winning combos
     const winningCombos = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
         [0, 3, 6], [1, 4, 7], [2, 5, 8],
         [0, 4, 8], [2, 4, 6]
     ]
-
+    // check if the gameboard has a combo with 3 circles
     winningCombos.forEach(array => {
         const circleWins = array.every(cell =>
             allSquares[cell].firstChild?.classList.contains("circle"))
@@ -52,6 +54,7 @@ function checkScore() {
             }
     })
 
+    // check if the gameboard has a combo with 3 crosses
     winningCombos.forEach(array => {
         const crossWins = array.every(cell =>
             allSquares[cell].firstChild?.classList.contains("cross"))
